e26f22086e3b068afe44420ed017c6f524fe9aa0
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2025-05-29 18:24:49 +0700

    fix: remove unused DIRECT_URL environment variable from server and runtime configurations

--- src/env.js
+++ src/env.js
@@ -8,7 +8,6 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
-    DIRECT_URL: z.string().url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
@@ -29,7 +28,6 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    DIRECT_URL: process.env.DIRECT_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
